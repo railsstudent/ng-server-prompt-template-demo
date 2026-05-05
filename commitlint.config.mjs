88bef6ede4c1afdfe61f5f57982974066958bcbe
Connie Leung <cleung334@gmail.com>
2026-05-06 07:41:20 +0800

    chore: install commitlint, husky, and lintstaged

---
+++ commitlint.config.mjs
@@ -0,0 +1,18 @@
+export default {
+  extends: ['@commitlint/config-angular'],
+  rules:  {
+    "type-enum": [2, 'always', [
+      "build",
+      "chore",
+      "ci",
+      "docs",
+      "feat",
+      "fix",
+      "perf",
+      "refactor",
+      "revert",
+      "style",
+      "test"
+    ]]
+  },
+};
