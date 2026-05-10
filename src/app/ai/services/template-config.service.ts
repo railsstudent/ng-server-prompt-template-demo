43190b2ddda49e76a09a9aa1f7dd04fbf758644e
Connie Leung <cleung334@gmail.com>
2026-05-10 17:38:33 +0800

    refactor: perform listener cleanup in the service

--- src/app/ai/services/template-config.service.ts
+++ src/app/ai/services/template-config.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, signal } from '@angular/core';
-import { getValue, RemoteConfig } from 'firebase/remote-config';
+import { DestroyRef, inject, Injectable, signal } from '@angular/core';
+import { activate, getValue, onConfigUpdate, RemoteConfig } from 'firebase/remote-config';
 import { TEMPLATE_KEYS } from '../constants/template-keys.const';
 import { TemplateKey, TemplateMap } from '../types/template-key.type';
 
@@ -17,6 +17,8 @@ export class TemplateConfigService {
   });
 
   templates = this.#templates.asReadonly();
+
+  destroyRef$ = inject(DestroyRef);
 
   getTemplateValue(key: TemplateKey) {
     if (key in this.#templates()) {
@@ -38,5 +40,47 @@ export class TemplateConfigService {
 
   updateTemplateId(key: TemplateKey, value: string) {
     this.#templates.update((prev) => ({ ...prev, [key]: value }));
+    console.log(`Update template id successfully.`, key, value);
+  }
+
+  setupRemoteConfigListener(remoteConfig: RemoteConfig) {
+    if (!remoteConfig) {
+      console.warn('⚠️ Remote Config is not initialized yet.');
+      throw new Error('Remote Config is not initialized yet.');
+    }
+
+    const unsubscribe = onConfigUpdate(remoteConfig, {
+      next: (configUpdate) => {
+        const updatedKeys = configUpdate.getUpdatedKeys();
+        const hasOverlap = TEMPLATE_KEYS.some((key) => updatedKeys.has(key));
+        if (hasOverlap) {
+          activate(remoteConfig).then(() => {
+            updatedKeys.forEach((key) => {
+              const value = getValue(remoteConfig, key).asString();
+              this.updateTemplateId(key as TemplateKey, value);
+            });
+          });
+        }
+      },
+      error: (error) => {
+        console.log('Config update error:', error);
+      },
+      complete: () => {
+        console.log('Listening stopped.');
+      },
+    });
+
+    this.destroyRef$.onDestroy(() => {
+      try {
+        unsubscribe();
+        console.log('Remote config listener unsubscribed successfully.');
+      } catch (e) {
+        const errMsg =
+          e instanceof Error
+            ? e.message
+            : 'Error occurs while unsubscribing remote config listener';
+        console.error(errMsg);
+      }
+    });
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { setupRemoteConfigListener } from './ai/listeners/remote-config.listener';
 import { ConfigService } from './ai/services/config.service';
-import { ServerPromptService } from './ai/services/server-prompt.service';
 import { TemplateConfigService } from './ai/services/template-config.service';
-import { GlobalStateService } from './ui/services/global-state.service';
 
 @Component({
   selector: 'app-root',
@@ -13,53 +10,17 @@ import { GlobalStateService } from './ui/services/global-state.service';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  protected readonly title = signal('ng-server-prompt-template-demo');
-  inlineImageData = signal('');
-
-  private readonly stateService = inject(GlobalStateService);
-
   configService = inject(ConfigService);
-  service = inject(ServerPromptService);
   remoteConfigService = inject(TemplateConfigService);
-  destroyRef$ = inject(DestroyRef);
-
-  isLoading = this.stateService.isLoading;
-  isError = this.stateService.isError;
-  errorMsg = this.stateService.errorMsg;
 
   constructor() {
     try {
-      this.isLoading.set(true);
-      this.isError.set(false);
-      this.errorMsg.set('');
       const remoteConfig = this.configService.remoteConfig;
-      const unsubscribe = setupRemoteConfigListener(remoteConfig, this.remoteConfigService);
-      this.destroyRef$.onDestroy(() => {
-        unsubscribe();
-        console.log('Remote config listener unsubscribed successfully.');
-      });
+      this.remoteConfigService.setupRemoteConfigListener(remoteConfig);
     } catch (e) {
       const errMsg =
         e instanceof Error ? e.message : 'Error occurs while setting up remote config listener';
-      this.isError.set(true);
-      this.errorMsg.set(errMsg);
       console.error(errMsg);
-    } finally {
-      this.isLoading.set(false);
-    }
-  }
-
-  async generateImage() {
-    try {
-      const templateId = this.remoteConfigService.getTemplateValue('countryTemplateId');
-      if (templateId) {
-        const inlineData = await this.service.generateContent(templateId, {
-          country: 'China',
-        });
-        this.inlineImageData.set(inlineData);
-      }
-    } catch (e) {
-      console.error(e);
     }
   }
 }
